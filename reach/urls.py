6ecc6f1e7d255e48ffaa628538a3012872b0cdd5
Zack Kourouma <zack@kourouma.me>
2015-06-18 11:15:19 -0400

    csrf in urls

--- reach/urls.py
+++ reach/urls.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import patterns, include, url
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import RedirectView
 
 from django.contrib import admin
@@ -12,6 +13,6 @@ from reachapp.views import EmailClickView, EmailEventView
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^emails/(?P<tracker_code>[a-zA-Z0-9]+)/(?P<tracker_log_code>[a-zA-Z0-9]+)/$', EmailClickView.as_view(), name='click_email'),
-    url(r'^events/$', EmailEventView.as_view(), name='email_event'),
+    url(r'^events/$', csrf_exempt(EmailEventView.as_view()), name='email_event'),
     (r'^$', RedirectView.as_view(url='/admin/')),
 )
